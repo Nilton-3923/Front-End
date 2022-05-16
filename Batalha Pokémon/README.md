65153efcb4550f07bc64473aa7e1f09f2f12700b
Nilton-3923 <64760713+Nilton-3923@users.noreply.github.com>
2022-05-15 23:54:44 -0300

    Pokemon

---
+++ Batalha Pokémon/README.md
@@ -0,0 +1,3 @@
+# Criando um Pokédex
+
+    Foi criado nesse repositório uma pokédex(do anime pokémon) usando HTML5, CSS3 e JavaScript. 
---
+++ Batalha Pokémon/joguinho.js
@@ -0,0 +1,17 @@
+
+    var a = 123;
+
+    function soma(b){
+        var numero = b
+        return numero;
+    }
+
+    var c = soma(a);
+
+    var contador = 3
+    do {
+        console.log(soma(contador))
+        contador -= 1
+    }
+    while(contador =! 0)
+
